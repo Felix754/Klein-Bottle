5c4e2c94940fb90311fe65d5eee263b45da9c55b
Vladyslav Oprelianskyi <WladyslawOprelanski@gmail.com>
2025-02-03 19:52:44 +0100

    Visual enhancements & additional object
    Added post-processing for lighting
    Modified attributes of some variables
    Changed orientation of all objects (by 90 degrees)
    Added skybox
    Improved code structure

--- main.js
+++ main.js
@@ -1,17 +1,37 @@
 import * as THREE from 'three';
 import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
-import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 import { ParametricGeometry } from 'three/examples/jsm/geometries/ParametricGeometry.js';
-import { emissive } from 'three/tsl';
+import { UnrealBloomPass } from 'three/examples/jsm/postprocessing/UnrealBloomPass.js';
+import { EffectComposer } from 'three/examples/jsm/postprocessing/EffectComposer.js';
+import { RenderPass } from 'three/examples/jsm/postprocessing/RenderPass.js';
 
 
+// textures/free_star_sky_hdri_spherical_map_by_kirriaa_dbw8p0w.jpg
 // scene,camera, render
 const scene = new THREE.Scene();
-scene.background = new THREE.Color(0x121212);
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
+renderer.toneMapping = THREE.ACESFilmicToneMapping;
+renderer.toneMappingExposure = 1;
+
+// postprosesing
+const composer = new EffectComposer(renderer);
+composer.addPass(new RenderPass(scene, camera));
+
+const bloomPass = new UnrealBloomPass(
+    new THREE.Vector2(window.innerWidth, window.innerHeight), 1.5, 0.2, 0.1);
+composer.addPass(bloomPass);
+
+
+// textures (./textures/LISENS.txt)
+const textureLoader = new THREE.TextureLoader();
+textureLoader.load('./textures/free_star_sky_hdri_spherical_map_by_kirriaa_dbw8p0w.jpg', function (texture) {
+    texture.mapping = THREE.EquirectangularReflectionMapping;
+    scene.background = texture;
+    scene.environment = texture;
+});
 
 
 // Klein Bottle func
@@ -38,23 +58,24 @@ const geometry = new ParametricGeometry(kleinBottle, 80, 40);
 const material = new THREE.MeshPhysicalMaterial({
     color: 0xffffff,
     transmission: 0.97,
-    thickness: 1,
+    thickness: 1.0,
     opacity: 1,
     roughness: 0.01,
     metalness: 0.1,
     ior: 1.52,
     clearcoat: 1,
     clearcoatRoughness: 0,
-    reflectivity: 1,
+    reflectivity: 0.1,
     side: THREE.DoubleSide
 });
+
 const kleinMesh = new THREE.Mesh(geometry, material);
 scene.add(kleinMesh);
 
 // creating geometry of balls
 const numBalls = 5;
 const spheres = [];
-const colors = [0x00A3FF, 0x8D33FF, 0xA1FF13, 0xFF338D, 0xFF9030]; 
+const colors = [0x00A3FF, 0x8D33FF, 0xA1FF13, 0xFF338D, 0xFF9030];
 
 
 for (let i = 0; i < numBalls; i++) {
@@ -62,32 +83,33 @@ for (let i = 0; i < numBalls; i++) {
     const sphereMaterial = new THREE.MeshStandardMaterial({
         color: colors[i],
         emissive: colors[i],
-        emissiveIntensity: 5,
+        emissiveIntensity: 10,
         roughness: 0.3,
         metalness: 0.8
     });
+
 
     const sphere = new THREE.Mesh(sphereGeometry, sphereMaterial);
     scene.add(sphere);
     spheres.push({ sphere, tOffset: i * 0.3 });
 
-    emissiveIntensity: 25;
-    const light = new THREE.PointLight(colors[i], 50, 20); 
+    const light = new THREE.PointLight(colors[i], 100, 50);
     sphere.add(light);
 }
 
 
 
-//light
-const light1 = new THREE.PointLight(0xffffff, 5, 100);
+
+//light & shadows
+const light1 = new THREE.PointLight(0xffffff, 10, 100);
 light1.position.set(10, 10, 10);
 scene.add(light1);
 
-const light2 = new THREE.PointLight(0xffffff, 3, 100);
+const light2 = new THREE.PointLight(0xffffff, 5, 100);
 light2.position.set(-10, -10, 10);
 scene.add(light2);
 
-const ambientLight = new THREE.AmbientLight(0xffffff, 0.3);
+const ambientLight = new THREE.AmbientLight(0xffffff, 0.8);
 scene.add(ambientLight);
 
 
@@ -96,10 +118,21 @@ camera.position.z = 25;
 const controls = new OrbitControls(camera, renderer.domElement);
 controls.enableDamping = true;
 
+const sceneGroup = new THREE.Group();
+scene.add(sceneGroup);
+
+// positons
+sceneGroup.add(kleinMesh);
+spheres.forEach(({ sphere }) => sceneGroup.add(sphere));
+sceneGroup.rotation.x = -(Math.PI / 2);
+sceneGroup.add(light1);
+sceneGroup.add(light2);
+
 // animation and rendering
 function animate() {
     requestAnimationFrame(animate);
     renderer.render(scene, camera);
+    composer.render();
 }
 
 let t = 0;
